2b378ccde515233b6df69c7bc158beb347bc546f
Suyash Vikram Singh <suyashvsingh2002@gmail.com>
2022-06-10 20:57:06 +0530

    added title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer({ onClickCheckAnswers, over, points }) {
     return (
         <div className="footer">
-            <button className='checkans' onClick={onClickCheckAnswers}>{`${over ? 'Restart' : 'Check answers'}`}</button>
+            <button className='checkans' onClick={onClickCheckAnswers}>{`${over ? 'Play again' : 'Check answers'}`}</button>
             {over && <div className="points">{`You scored ${points} points`}</div>}
         </div>
     )
